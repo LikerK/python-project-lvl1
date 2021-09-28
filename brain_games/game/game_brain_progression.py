10683de9f3f9901306453ad7393a96b15ad21111
Kirill <nagorniykirill@gmail.com>
2021-09-28 12:11:01 +0400

    add game arithmetic progression, game/brain_progression.py

---
+++ brain_games/game/game_brain_progression.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from random import randint
+
+
+task_game = 'What number is missing in the progression?'
+
+
+def get_game_round():
+    amount_number = randint(5, 10)
+    start_number_progession = randint(1, 20)
+    arithmetic_progression = randint(2, 10)
+    result = []
+    for i in range(amount_number):
+        result.append(start_number_progession)
+        start_number_progession += arithmetic_progression
+    random_index = randint(0, len(result) - 1)
+    correct_answer = result[random_index]
+    result[random_index] = '..'
+    result = ' '.join([str(a) for a in result])
+    return result, str(correct_answer)
