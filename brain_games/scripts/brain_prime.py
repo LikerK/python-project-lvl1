193285782cd3b24a5599aed1c509690bfeeb4dc4
Kirill <nagorniykirill@gmail.com>
2021-09-28 12:39:31 +0400

    add game brain-prime

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from brain_games.game import game_brain_prime
+from brain_games.game_logic import get_game
+
+
+def main():
+    get_game(game_brain_prime)
+
+
+if __name__ == "__main__":
+    main()
